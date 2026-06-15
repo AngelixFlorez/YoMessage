958d62137b93321366108127ec93fd1fbe97cf35
speedy <angelixflorez@gmail.com>
2026-06-15 16:24:52 -0500

    feat: initialize express server with database connection, clerk middleware, and static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,30 +2,27 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
-import User from "./models/user.model.js";
-import { clerkMiddleware, clerkRequest } from "@clerk/express";
-import fs from 'fs';
-import path from 'path';
+import { clerkMiddleware } from "@clerk/express";
+import fs from 'node:fs';
+import path from 'node:path';
 
-app.use(express.json());
-
-const PORT = process.env.PORT || 3100;
 const app = express();
+const PORT = process.env.PORT || 3100;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 const publicDir = path.join(process.cwd(), "public");
 
-if(!fs.existsSync(publicDir))  {
-   app.use(express.static(publicDir));
-   app.get("/{*any}", (req, res, next)=> res.sendFile(path.join(publicDir, "index.html")));
-};
-
-
+app.use(express.json());
 app.use(cors({origin: FRONTEND_URL, credentials: true}));
 app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
     res.status(200).json({ok:true});
 });
+
+if (fs.existsSync(publicDir)) {
+   app.use(express.static(publicDir));
+   app.get("/{*any}", (req, res) => res.sendFile(path.join(publicDir, "index.html")));
+}
 
 app.listen(PORT, () => {
     connectDB();
