754cf648fe6d1ed2ed74571602afad478ddff8e5
speedy <angelixflorez@gmail.com>
2026-06-15 15:08:31 -0500

    feat: initialize backend server, Docker configuration, and multi-stage containerization build process

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,21 @@ import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import User from "./models/user.model.js";
 import { clerkMiddleware, clerkRequest } from "@clerk/express";
-
-
+import fs from 'fs';
+import path from 'path';
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3100;
 const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL;
+const publicDir = path.join(process.cwd(), "public");
+
+if(!fs.existsSync(publicDir))  {
+   app.use(express.static(publicDir));
+   app.get("/{*any}", (req, res, next)=> res.sendFile(path.join(publicDir, "index.html")));
+};
+
 
 app.use(cors({origin: FRONTEND_URL, credentials: true}));
 app.use(clerkMiddleware());
